df46a0314bd5af407dc72e951a3eacb93974a7eb
kakashi250802 <ngoduong37na@gmail.com>
2021-07-18 00:51:37 +0700

    update pc ui

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.dropdown > a').forEach(e => {
+    e.addEventListener('click', (event) => event.preventDefault())
+});
+document.querySelectorAll('.mega-dropdown > a').forEach(e => {
+    e.addEventListener('click', (event) => event.preventDefault())
+});
