992f0010ccf4ef392b7c655cb2dd17a94d1f718b
kakashi250802 <ngoduong37na@gmail.com>
2021-07-18 17:53:49 +0700

    update full responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,5 @@ document.querySelectorAll('.dropdown > a').forEach(e => {
 document.querySelectorAll('.mega-dropdown > a').forEach(e => {
     e.addEventListener('click', (event) => event.preventDefault())
 });
+document.querySelector('#mb-menu-toggle').addEventListener('click', () => document.querySelector('#header-wrapper').classList.add('active'));
+document.querySelector('#mb-menu-close').addEventListener('click', () => document.querySelector('#header-wrapper').classList.remove('active'));
